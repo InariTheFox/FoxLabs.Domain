b53bb9b7447c72b4218377ce6b679d2fe9be3655
Brandon Dobbie <brandon@fox-labs.io>
2024-06-18 17:10:17 -0400

    Adjusted the interfaces and base classes to properly allow for non-generic patterns. #2

--- src/Foxlabs.Domain.Abstractions/IAggregateRoot.cs
+++ src/Foxlabs.Domain.Abstractions/IAggregateRoot.cs
@@ -1,40 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace FoxLabs.Domain
 {
     /// <summary>
     /// The basic interface for an aggregate root entity.
     /// </summary>
-    public interface IAggregateRoot : IAggregateRoot<int>
-    { }
+    public interface IAggregateRoot
+    { 
+        long Version { get; }
+    }
 
     /// <summary>
     /// The basic interface for an aggregate root entity.
     /// </summary>
-    public interface IAggregateRoot<TKey> : IEntity<TKey>
+    public interface IAggregateRoot<TKey> : IEntity<TKey>, IAggregateRoot
         where TKey : IComparable
     {
-        /// <summary>
-        /// A collection of <see cref="IDomainEvent" />s.
-        /// </summary>
-        IReadOnlyCollection<IDomainEvent<TKey>> DomainEvents { get; }
-
-        long Version { get; }
-
-        /// <summary>
-        /// Adds an <see cref="IDomainEvent" /> to the entity.
-        /// </summary>
-        void AddDomainEvent(IDomainEvent<TKey> @event);
-
-        /// <summary>
-        /// Clears the collection of <see cref="IDomainEvent" />s on the entity.
-        /// </summary>
-        void ClearDomainEvents();
-
-        /// <summary>
-        /// Remove an <see cref="IDomainEvent" /> from the entity.
-        /// </summary>
-        void RemoveDomainEvent(IDomainEvent<TKey> @event);
     }
 }
--- src/Foxlabs.Domain.Abstractions/IDomainEvent.cs
+++ src/Foxlabs.Domain.Abstractions/IDomainEvent.cs
@@ -6,16 +6,22 @@ namespace FoxLabs.Domain
     /// <summary>
     /// The basic interface for a domain event.
     /// </summary>
-    /// <typeparam name="TKey">The entity key type.</typeparam>
-    public interface IDomainEvent<TKey> : INotification
-        where TKey : IComparable
+    public interface IDomainEvent : INotification
     {
         IAggregateRoot RootAggregate { get; }
-
-        TKey RootId { get; }
 
         long RootVersion { get; }
 
         DateTimeOffset Timestamp { get; }
+    }
+
+    /// <summary>
+    /// The basic interface for a domain event.
+    /// </summary>
+    /// <typeparam name="TKey">The entity key type.</typeparam>
+    public interface IDomainEvent<TKey> : IDomainEvent
+        where TKey : IComparable
+    {
+        TKey RootId { get; }
     }
 }
